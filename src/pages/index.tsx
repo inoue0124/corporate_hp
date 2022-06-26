172c706a7037f117a80b6eec7693b9a235be6922
inoue0124 <alice7124@gmail.com>
2022-06-27 03:23:28 +0900

    Implement waveform

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,24 @@
 import Ellipse from '@/components/ellipse';
 import Header from '@/components/header';
-import { ChakraProvider, Fade, keyframes } from '@chakra-ui/react';
+import WaveForm from '@/components/waveform';
+import {
+  Box,
+  Center,
+  ChakraProvider,
+  Fade,
+  HStack,
+  Spacer,
+  Text,
+} from '@chakra-ui/react';
 import theme from '../theme';
 
 export default function Home() {
   return (
     <>
       <ChakraProvider theme={theme}>
-        <Header />
+        <Center>
+          <Header w={'1000px'} />
+        </Center>
         <main>
           <Fade in={true}>
             <Ellipse
@@ -18,9 +29,24 @@ export default function Home() {
               top="-200px"
             />
           </Fade>
-          <Fade in={true}>
-            <Ellipse w="250px" h="250px" />
-          </Fade>
+
+          <Center as={'section'}>
+            <HStack height={'70vh'} width={'1000px'}>
+              <Text color="grey.900" fontSize={36}>
+                ワクワクする未来を創る人材を
+                <br />
+                キャリーする
+              </Text>
+              <Spacer />
+              <WaveForm height={'80%'} />
+            </HStack>
+          </Center>
+
+          <Box as={'section'} position={'relative'}>
+            <Fade in={true}>
+              <Ellipse w="250px" h="250px" />
+            </Fade>
+          </Box>
         </main>
       </ChakraProvider>
     </>
