5449ca63342f795a9f1864af30e868b5b88db127
inoue0124 <alice7124@gmail.com>
2022-06-27 10:43:29 +0900

    Delete title component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,6 @@
-import Title from '@/components/Title';
-
 export default function Home() {
   return (
     <main>
-      <Title>Hello TypeScript!</Title>
       <p>A TypeScript starter for Gatsby. Great for advanced users.</p>
       <p>
         Follow me on Twitter (
